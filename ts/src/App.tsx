08944367a53d3d833432ae0a4b847d192ac1a791
mAndrushchenko <andr.mv97@gamil.com>
2021-04-29 11:58:16 +0300

    images path changed

--- ts/src/App.tsx
+++ ts/src/App.tsx
@@ -4,7 +4,6 @@ import { Input } from "./components/Input"
 import { List } from "./components/List"
 import { Provider } from "react-redux"
 import { store } from "./store"
-import image from './css/background.jpg'
 import './css/index.css'
 
 export const App: FC = () => {
@@ -12,7 +11,7 @@ export const App: FC = () => {
         <Provider store={store}>
             <ReactNotification className="notification"/>
             <div className="todos">
-                <img src={image} className="bg" alt="" draggable={false}/>
+                <img src={process.env.PUBLIC_URL + '/background.jpg'} className="bg" alt="" draggable={false}/>
                 <div className="by-hooks">
                     <Input/>
                     <List/>
--- ts/src/components/notifacation/ErrorIcon.tsx
+++ ts/src/components/notifacation/ErrorIcon.tsx
@@ -1,13 +1,12 @@
 import React, { FC } from "react"
 import { IErrorIcon } from "../../types/components-types"
-import icon from '../../css/false-2061131_640.png'
 import '../../css/index.css'
 
 
 export const ErrorIcon: FC<IErrorIcon> = ({ message }) => {
     return (
         <div className="custom-icon">
-            <img className="error-icon" src={icon} alt="" draggable={false}/>
+            <img className="error-icon" src={process.env.PUBLIC_URL + '/cross.png'} alt="" draggable={false}/>
             <p className="message">{message}</p>
         </div>
     )
